866ee54bcb89f3dcec0385e17c487bce8e4db052
meta-panic <ritaritarita2013@gmail.com>
2025-05-13 00:11:19 +0400

    refactor: remove commented-out code for database and user services initialization

--- src/index.ts
+++ src/index.ts
@@ -12,14 +12,3 @@ if (args === "single") {
 } else {
   console.error("Incorrect app arguments:", args);
 }
-
-// const dbService = mockDBService();
-// const DB_PORT: string | undefined = process.env.DB_PORT || "1234";
-
-// const userService = new UserService(`http://localhost:${DB_PORT}`);
-// const userController = new UserController(userService);
-
-// new App()
-//   .registerRoutes(userController.routers)
-//   .initDB(dbService, DB_PORT) // TODO: rename method
-//   .startServer();
